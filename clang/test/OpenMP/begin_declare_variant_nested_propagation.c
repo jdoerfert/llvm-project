d36a2fe6341e24339bb3d41c0d6af937498a6aee
Johannes Doerfert <johannes@jdoerfert.de>
2021-01-31 11:32:29 -0600

    [OpenMP] Introduce the `disable_selector_propagation` variant selector trait
    Nested `omp [begin|end] declare variant` inherit the selectors from
    surrounding `omp (begin|end) declare variant` constructs. To stop such
    propagation the user can add the `disable_selector_propagation` to the
    `extension` set in the `implementation` selector.
    
    Differential Revision: https://reviews.llvm.org/D95765

--- clang/test/OpenMP/begin_declare_variant_nested_propagation.c
+++ clang/test/OpenMP/begin_declare_variant_nested_propagation.c
@@ -1,8 +1,22 @@
 // RUN: %clang_cc1 -triple=x86_64-pc-win32 -verify -fopenmp -x c -std=c99 -fms-extensions -Wno-pragma-pack %s
-// expected-no-diagnostics
 
 #pragma omp begin declare variant match(implementation={extension(match_any)})
 #pragma omp begin declare variant match(device = {vendor(cray, ibm)})
  this is never reached, we cannot have a cray ibm compiler hybrid, I hope.
 #pragma omp end declare variant
 #pragma omp end declare variant
+
+#pragma omp begin declare variant match(implementation={extension(disable_implicit_base, disable_selector_propagation)})
+
+ void without_implicit_base() {}
+
+#pragma omp begin declare variant match(implementation = {vendor(llvm)})
+ void with_implicit_base() {}
+#pragma omp end declare variant
+
+#pragma omp end declare variant
+
+ void test() {
+   without_implicit_base(); // expected-warning{{implicit declaration of function 'without_implicit_base' is invalid in C99}}
+   with_implicit_base();
+ }
