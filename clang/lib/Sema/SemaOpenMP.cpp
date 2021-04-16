ec3a39d76bc641103797b1a0790fed8c8f6fa39f
Johannes Doerfert <johannes@jdoerfert.de>
2021-04-16 00:44:50 -0500

    [OpenMP] Ensure the DefaultMapperId has a location
    A user reported an assertion (below) but without a reproducer. I failed to
    create a test myself but from the assertion one can derive the problem.
    I set the DefaultMapperId location now to make sure this doesn't cause
    trouble.
    
    ```
    clang-13: .../DeclTemplate.h:1940:
    void clang::ClassTemplateSpecializationDecl::setPointOfInstantiation(clang::SourceLocation):
    Assertion `Loc.isValid() && "point of instantiation must be valid!"' failed.
    ```
    
    Differential Revision: https://reviews.llvm.org/D100621

--- clang/lib/Sema/SemaOpenMP.cpp
+++ clang/lib/Sema/SemaOpenMP.cpp
@@ -5557,9 +5557,6 @@ processImplicitMapsWithDefaultMappers(Sema &S, DSAStackTy *Stack,
   if (S.getLangOpts().OpenMP < 50)
     return;
   SmallVector<OMPClause *, 4> ImplicitMaps;
-  DeclarationNameInfo DefaultMapperId;
-  DefaultMapperId.setName(S.Context.DeclarationNames.getIdentifier(
-      &S.Context.Idents.get("default")));
   for (int Cnt = 0, EndCnt = Clauses.size(); Cnt < EndCnt; ++Cnt) {
     auto *C = dyn_cast<OMPMapClause>(Clauses[Cnt]);
     if (!C)
@@ -5615,6 +5612,10 @@ processImplicitMapsWithDefaultMappers(Sema &S, DSAStackTy *Stack,
         if (It == Visited.end()) {
           // Try to find the associated user-defined mapper.
           CXXScopeSpec MapperIdScopeSpec;
+          DeclarationNameInfo DefaultMapperId;
+          DefaultMapperId.setName(S.Context.DeclarationNames.getIdentifier(
+                                  &S.Context.Idents.get("default")));
+          DefaultMapperId.setLoc(E->getExprLoc());
           ExprResult ER = buildUserDefinedMapperRef(
               S, Stack->getCurScope(), MapperIdScopeSpec, DefaultMapperId,
               BaseType, /*UnresolvedMapper=*/nullptr);
