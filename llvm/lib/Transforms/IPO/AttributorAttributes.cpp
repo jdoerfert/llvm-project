8034037d0b0960d8fb4d461ecc8c67a993535e82
Johannes Doerfert <johannes@jdoerfert.de>
2021-05-06 16:51:37 -0500

    [Attributor][FIX] Ensure we replace undef if we see the first "real" value
    The state of AAPotentialValues tracks if undef is contained. It should
    fold undef into the first non-undef value. However we missed a case
    before. There was also a shadowing definition of two variables that
    caused trouble. The test exposes both problems.

--- llvm/lib/Transforms/IPO/AttributorAttributes.cpp
+++ llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -7728,7 +7728,6 @@ struct AAPotentialValuesFloating : AAPotentialValuesImpl {
       // with undef.
       unionAssumedWithUndef();
     } else if (LHSAA.undefIsContained()) {
-      bool MaybeTrue = false, MaybeFalse = false;
       for (const APInt &R : RHSAAPVS) {
         bool CmpResult = calculateICmpInst(ICI, Zero, R);
         MaybeTrue |= CmpResult;
