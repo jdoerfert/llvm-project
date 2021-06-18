ee0eaa6c285248692cff1c7cb6e1fda9661f26bd
Johannes Doerfert <johannes@jdoerfert.de>
2021-06-17 19:01:22 -0500

    [Attributor] Allow to skip the initial update for a new AA
    Users might want to run initialize for a set of AAs without an
    intermediate update step. Running update eagerly is not a requirement
    anyway so we make it optional.

--- llvm/include/llvm/Transforms/IPO/Attributor.h
+++ llvm/include/llvm/Transforms/IPO/Attributor.h
@@ -1143,9 +1143,10 @@ struct Attributor {
   /// function.
   /// NOTE: ForceUpdate is ignored in any stage other than the update stage.
   template <typename AAType>
-  const AAType &
-  getOrCreateAAFor(IRPosition IRP, const AbstractAttribute *QueryingAA,
-                   DepClassTy DepClass, bool ForceUpdate = false) {
+  const AAType &getOrCreateAAFor(IRPosition IRP,
+                                 const AbstractAttribute *QueryingAA,
+                                 DepClassTy DepClass, bool ForceUpdate = false,
+                                 bool UpdateAfterInit = true) {
     if (!shouldPropagateCallBaseContext(IRP))
       IRP = IRP.stripCallBaseContext();
 
@@ -1212,12 +1213,14 @@ struct Attributor {
 
     // Allow seeded attributes to declare dependencies.
     // Remember the seeding state.
-    AttributorPhase OldPhase = Phase;
-    Phase = AttributorPhase::UPDATE;
+    if (UpdateAfterInit) {
+      AttributorPhase OldPhase = Phase;
+      Phase = AttributorPhase::UPDATE;
 
-    updateAA(AA);
+      updateAA(AA);
 
-    Phase = OldPhase;
+      Phase = OldPhase;
+    }
 
     if (QueryingAA && AA.getState().isValidState())
       recordDependence(AA, const_cast<AbstractAttribute &>(*QueryingAA),
