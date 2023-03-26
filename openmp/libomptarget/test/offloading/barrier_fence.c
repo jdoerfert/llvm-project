26bf19250314bf91377a24d73f62e0ded51471b7
Johannes Doerfert <johannes@jdoerfert.de>
2023-03-26 00:17:35 -0700

    [OpenMP] Enable more runtime tests and also run them with O3
    The test run fine on my AMD GPU machine, we should verify them on others
    too and put them into our regular testing. Not testing O1/2/3 is really
    bad and not testing all architecturs is similarly problematic.

--- openmp/libomptarget/test/offloading/barrier_fence.c
+++ openmp/libomptarget/test/offloading/barrier_fence.c
@@ -1,5 +1,7 @@
 // RUN: %libomptarget-compile-generic -fopenmp-offload-mandatory -O3
 // RUN: %libomptarget-run-generic
+// RUN: %libomptarget-compileopt-generic -fopenmp-offload-mandatory -O3
+// RUN: %libomptarget-run-generic
 
 // UNSUPPORTED: x86_64-pc-linux-gnu
 // UNSUPPORTED: x86_64-pc-linux-gnu-LTO
