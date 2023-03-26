005eccd24c63a438bbd14f1d9100f5fda33730e4
Johannes Doerfert <johannes@jdoerfert.de>
2023-03-26 00:17:43 -0700

    Test unknown

--- openmp/libomptarget/test/mapping/declare_mapper_nested_default_mappers_array.cpp
+++ openmp/libomptarget/test/mapping/declare_mapper_nested_default_mappers_array.cpp
@@ -1,10 +1,5 @@
-// RUN: %libomptarget-compilexx-run-and-check-aarch64-unknown-linux-gnu
-// RUN: %libomptarget-compilexx-run-and-check-powerpc64-ibm-linux-gnu
-// RUN: %libomptarget-compilexx-run-and-check-powerpc64le-ibm-linux-gnu
-// RUN: %libomptarget-compilexx-run-and-check-x86_64-pc-linux-gnu
-// RUN: %libomptarget-compilexx-run-and-check-nvptx64-nvidia-cuda
-
-// UNSUPPORTED: clang
+// RUN: %libomptarget-compilexx-run-and-check-generic
+// RUN: %libomptarget-compileoptxx-run-and-check-generic
 
 #include <cstdio>
 #include <cstdlib>
@@ -53,17 +48,22 @@ int main() {
          sa[1].f.b == &x[0] ? 1 : 0);
   // CHECK: 111 222 777 20.00000 1
 
+  int sa1fa = -1, sa1fca = -1, sa1fb_r = -1;
   __intptr_t p = reinterpret_cast<__intptr_t>(&y[0]);
-#pragma omp target map(tofrom : sa) firstprivate(p)
+#pragma omp target map(tofrom                                                  \
+                       : sa) firstprivate(p) map(from                          \
+                                                 : sa1fa, sa1fca, sa1fb_r)
   {
-    printf("%d %d %d\n", sa[1].f.a, sa[1].f.c.a,
-           sa[1].f.b == reinterpret_cast<void *>(p) ? 1 : 0);
-    // CHECK: 222 777 0
+    sa1fa = sa[1].f.a;
+    sa1fca = sa[1].f.c.a;
+    sa1fb_r = sa[1].f.b == reinterpret_cast<void *>(p) ? 1 : 0;
     sa[1].e = 333;
     sa[1].f.a = 444;
     sa[1].f.c.a = 555;
     sa[1].f.b[1] = 40;
   }
+  printf("%d %d %d\n", sa1fa, sa1fca, sa1fb_r);
+  // CHECK: 222 777 0
   printf("%d %d %d %4.5f %d\n", sa[1].e, sa[1].f.a, sa[1].f.c.a, sa[1].f.b[1],
          sa[1].f.b == &x[0] ? 1 : 0);
   // CHECK: 333 222 777 40.00000 1
