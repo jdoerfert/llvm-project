12cf5ff25a64c8c3861d7ac9eb71a269ff6b9f90
Johannes Doerfert <johannes@jdoerfert.de>
2025-04-06 17:10:22 -0700

    Replace per-module ctors with one global ctor (untested)
    This should resolve the ordering issue between module constructors

--- compiler-rt/lib/objsan/objsan_rt.cpp
+++ compiler-rt/lib/objsan/objsan_rt.cpp
@@ -1,5 +1,4 @@
 #include "include/obj_encoding.h"
-
 namespace __objsan {
 
 __attribute__((visibility("default"))) SmallObjectsTy SmallObjects;
@@ -13,4 +12,18 @@ __attribute__((visibility("default"))) StatsTy SRange("range");
 __attribute__((visibility("default"))) StatsTy SLoopR("loopr");
 #endif
 #endif
+
 } // namespace __objsan
+
+extern "C" {
+using CtorFn = void (*)(void);
+extern CtorFn *__start___objsan_ctor;
+extern CtorFn *__stop___objsan_ctor;
+
+__attribute__((constructor(1000))) void __objsan_ctor_init() {
+//  printf("CTOR INIT %lu\n", __stop__objsan_ctor - __start__objsan_ctor);
+  for (CtorFn *Ctor = __start___objsan_ctor, *E = __stop___objsan_ctor; Ctor != E;
+       ++Ctor)
+    (*Ctor)();
+}
+}
