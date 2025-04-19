4631a50301703f165a4558c7283184c33b9a242e
Kevin Sala <salapenades1@llnl.gov>
2025-04-18 20:43:00 -0700

    [Instrumentor] Add Instrumentor pass
    This is only a squash commit of all contributions to the
    Instrumentor pass. The following are the authors and
    contributors of this pass:
    
    Co-authored-by: Johannes Doerfert <johannes@jdoerfert.de>
    Co-authored-by: Kevin Sala <salapenades1@llnl.gov>
    Co-authored-by: Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
    Co-authored-by: Ethan Luis McDonough <ethanluismcdonough@gmail.com>

--- llvm/include/llvm/Analysis/MemoryBuiltins.h
+++ llvm/include/llvm/Analysis/MemoryBuiltins.h
@@ -135,6 +135,23 @@ LLVM_ABI Constant *getInitialValueOfAllocation(const Value *V,
 LLVM_ABI std::optional<StringRef>
 getAllocationFamily(const Value *I, const TargetLibraryInfo *TLI);
 
+/// Description of an allocation call. Note that some elements might be null.
+struct AllocationCallInfo {
+  /// The name of the allocation family, e.g., malloc.
+  std::optional<StringRef> Family;
+  /// The known initial value, usually 0, UndefValue, or unknown (=nullptr).
+  Constant *InitialValue;
+  /// The total size is the product of SizeLHS and SizeRHS, if both are
+  /// non-null, otherwise it is simply the non-null value.
+  int SizeLHSArgNo, SizeRHSArgNo;
+  /// The statically requested alignment.
+  int AlignmentArgNo;
+};
+
+/// Return all known information about the allocation call \p CB.
+std::optional<AllocationCallInfo>
+getAllocationCallInfo(const CallBase *CB, const TargetLibraryInfo *TLI);
+
 //===----------------------------------------------------------------------===//
 //  Utility functions to compute size of objects.
 //
--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -64,6 +64,7 @@
 #include "llvm/Transforms/IPO/IROutliner.h"
 #include "llvm/Transforms/IPO/InferFunctionAttrs.h"
 #include "llvm/Transforms/IPO/Inliner.h"
+#include "llvm/Transforms/IPO/Instrumentor.h"
 #include "llvm/Transforms/IPO/LowerTypeTests.h"
 #include "llvm/Transforms/IPO/MemProfContextDisambiguation.h"
 #include "llvm/Transforms/IPO/MergeFunctions.h"
@@ -220,6 +221,19 @@ static cl::opt<bool> EnableUnrollAndJam("enable-unroll-and-jam",
 static cl::opt<bool> EnableLoopFlatten("enable-loop-flatten", cl::init(false),
                                        cl::Hidden,
                                        cl::desc("Enable the LoopFlatten Pass"));
+
+static cl::opt<bool>
+    EnableInstrumentor("enable-instrumentor", cl::init(false), cl::Hidden,
+                       cl::desc("Enable the Instrumentor Pass"));
+
+// Experimentally allow loop header duplication. This should allow for better
+// optimization at Oz, since loop-idiom recognition can then recognize things
+// like memcpy. If this ends up being useful for many targets, we should drop
+// this flag and make a code generation option that can be controlled
+// independent of the opt level and exposed through the frontend.
+static cl::opt<bool> EnableLoopHeaderDuplication(
+    "enable-loop-header-duplication", cl::init(false), cl::Hidden,
+    cl::desc("Enable loop header duplication at any optimization level"));
 
 static cl::opt<bool>
     EnableDFAJumpThreading("enable-dfa-jump-thread",
@@ -1641,6 +1655,10 @@ PassBuilder::buildModuleOptimizationPipeline(OptimizationLevel Level,
 
   invokeOptimizerLastEPCallbacks(MPM, Level, LTOPhase);
 
+  // Run the Instrumentor pass late.
+  if (EnableInstrumentor)
+    MPM.addPass(InstrumentorPass());
+
   // Split out cold code. Splitting is done late to avoid hiding context from
   // other optimizations and inadvertently regressing performance. The tradeoff
   // is that this has a higher code size cost than splitting early.
@@ -2418,6 +2436,9 @@ PassBuilder::buildO0DefaultPipeline(OptimizationLevel Level,
     MPM.addPass(AllocTokenPass());
 
   invokeOptimizerLastEPCallbacks(MPM, Level, Phase);
+
+  if (EnableInstrumentor)
+    MPM.addPass(InstrumentorPass());
 
   if (isLTOPreLink(Phase))
     addRequiredLTOPreLinkPasses(MPM);
