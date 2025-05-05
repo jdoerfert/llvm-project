a6a2185024416a1d178c8e9d22ff4a8e55ff9f26
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-05-05 20:58:00 +0900

    WIP Features

--- llvm/include/llvm/Analysis/UnrollModelFeatureMaps.h
+++ llvm/include/llvm/Analysis/UnrollModelFeatureMaps.h
@@ -54,63 +54,15 @@ constexpr auto AccessAlignmentsBins = makeArray({
 });
 constexpr unsigned AccessAlignmentsBinsNum = AccessAlignmentsBins.size() + 2;
 
-// TODO
 constexpr auto SpacialReuseDistanceBins = makeArray({
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    12,
-    16,
-    20,
-    24,
-    32,
-    40,
-    48,
-    56,
-    64,
+    0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18,
+    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 40, 48, 56, 64,
 });
 constexpr unsigned SpacialReuseDistanceBinsNum =
     SpacialReuseDistanceBins.size() + 2;
 
-// TODO
 constexpr auto PtrStridesBins = makeArray({
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    12,
-    16,
-    20,
-    24,
-    32,
-    40,
-    48,
-    56,
-    64,
-//     1,
-//     2,
-//     4,
-//     8,
-//     16,
-//     24,
-//     32,
-//     40,
-//     48,
-//     56,
-//     64,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 16, 20, 24, 32, 40, 48, 56, 64,
 });
 constexpr unsigned PtrStridesBinsNum = PtrStridesBins.size() + 2;
 
