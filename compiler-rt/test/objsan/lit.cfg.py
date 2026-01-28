799005f842baea620d8f7850bfad9ca2bc6ce53b
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2026-01-28 23:57:33 +0900

    Rudimentary testing config
    can be run using
    
    cmake --build $LLVM_BUILD_DIR/runtimes/runtimes-bins/ -t check-objsan

--- compiler-rt/test/objsan/lit.cfg.py
+++ compiler-rt/test/objsan/lit.cfg.py
@@ -4,36 +4,17 @@ import os
 import subprocess
 
 # Setup config name.
-config.name = "input-gen" + config.name_suffix
+config.name = "objsan"
 
 # Setup source root.
 config.test_source_root = os.path.dirname(__file__)
 
-config.suffixes = [".c", ".cpp"]
-if config.host_os not in ["Linux"]:
-    config.unsupported = True
+config.suffixes = [".c", ".cpp", ".cu"]
 
-def cxx_build_invocation(compile_flags, with_lto=False):
-    lto_flags = []
-    if with_lto and config.lto_supported:
-        lto_flags += config.lto_flags
-
-    return " " + " ".join([config.clang] + config.cxx_mode_flags + lto_flags + compile_flags) + " "
-
-gen_args = '-mllvm --input-gen-mode=generate -flto -O2'.split(' ')
-replay_gen_args = '-mllvm --input-gen-mode=replay_generated -flto -O2'.split(' ')
-
-etc_args = []
-
-gen_args_link = '-fuse-ld=lld -flto -linputgen.generate -lpthread -lstdc++ -O2'.split(' ') + etc_args
-replay_gen_args_link = '-fuse-ld=lld -flto -linputgen.replay -lpthread -lstdc++ -O2'.split(' ') + etc_args
+cxx_cuda_args = config.clang + '-x cuda -fsanitize=object -mllvm -objsan-gpu-only=1 -mllvm -objsan-runtime-bitcode -mllvm objsan_ir_rt.bc -fgpu-rdc -foffload-lto --offload-link -Xoffload-linker objsan_rt.o'
 
 config.substitutions.append(
-    ('%clangxx_inputgen_gen', cxx_build_invocation(gen_args)))
-config.substitutions.append(
-    ('%clangxx_inputgen_replay_gen', cxx_build_invocation(replay_gen_args)))
+    ('%clangxx_objsan_cuda', cxx_cuda_args))
 
 config.substitutions.append(
-    ('%clangxx_inputgen_link_gen', cxx_build_invocation(gen_args_link)))
-config.substitutions.append(
-    ('%clangxx_inputgen_link_replay_gen', cxx_build_invocation(replay_gen_args_link)))
+    ('%cuda_env', ''))
